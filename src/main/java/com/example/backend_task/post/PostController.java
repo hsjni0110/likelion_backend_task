cce414ed4b7f59409f3467b340191fd76918bd1c
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-05-08 22:34:46 +0900

    feat : update 구현

--- src/main/java/com/example/backend_task/post/PostController.java
+++ src/main/java/com/example/backend_task/post/PostController.java
@@ -3,11 +3,7 @@ package com.example.backend_task.post;
 import com.example.backend_task.auth.Auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -25,6 +21,15 @@ public class PostController {
     ) {
         Long PostId = postService.save(request.title(), request.content(), userId);
         return ResponseEntity.created(URI.create("/posts/" + PostId)).build();
+    }
+    // 게시글 수정
+    @PutMapping("/posts/{postId}")
+    public void update(
+            @Auth Long memberId,
+            @PathVariable("postId") Long postId,
+            @RequestBody PostUpdateRequest request
+    ) {
+        postService.update(postId, memberId, request.title(), request.content());
     }
 
     @GetMapping("/posts/{postId}")
