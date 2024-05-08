bdf46dec468311dd87de4ae06ed27871473b5f47
minji <qkralswl6125@naver.com>
2024-05-08 16:41:06 +0900

    feat(회원가입) : MemberController와 MemberService생성, 회원가입 기능 구현

--- src/main/java/com/example/backend_task/member/SignupRequest.java
+++ src/main/java/com/example/backend_task/member/SignupRequest.java
@@ -1,5 +1,10 @@
 package com.example.backend_task.member;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class SignupRequest {
     private String username;
     private String password;
